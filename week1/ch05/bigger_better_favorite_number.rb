cc8a0ed07845190aabd1d5b2e87788922f873ca2
kenhinh1 <kenhinh@berkeley.edu>
2018-06-15 20:07:55 -0700

    Complete minutes_in_decade exercise

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,5 @@
+puts 'What is your favorite number?'
+favoritenumber = gets.chomp
+fave = favoritenumber.to_i
+best_number = fave + 1
+puts 'Stanford likes that number. How about a better and bigger favorite number of ' + best_number.to_s + '?'
---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,9 @@
+puts 'Hello there, and what\'s your first name?'
+first = gets.chomp
+puts 'Your name is ' + first + '? What a lovely name!'
+puts 'What is your middle name, ' + 'name' + '?'
+middle = gets.chomp
+puts 'Your middle name is ' + middle + '?'
+puts 'Now, what is your last name?'
+last = gets.chomp
+puts 'Thank you, ' + first + ' ' + middle + ' ' + last
